f22e683d1295f634b80a09a21153f6bc2998532c
deadwi <deadwi@gmail.com>
2017-03-23 16:40:33 +0900

    Update Names.java

--- src/main/java/example/hive/serde/Names.java
+++ src/main/java/example/hive/serde/Names.java
@@ -2,6 +2,6 @@ package example.hive.serde;
 
 public class Names
 {
-	public static final String TABLE_NAME = "__DP_TABLE_NAME__";
+	public static final String TABLE_NAME = "__TABLE_NAME__";
 	public static final String SCHEMA_NAME = "input.schema.name";
 }
